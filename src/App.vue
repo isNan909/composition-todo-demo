7ea7b3ce675cd92efe0c93b5e59eeabb62d5b8f4
Ishan Manandhar <ishan02016@gmail.com>
2019-11-03 15:38:17 +0545

    using old options API

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="app">
-    <h1>Hello World</h1>
+    <section>
+      <input type="text" v-model="todo" />
+      <ul>
+        <li v-for="(todo, i) in todolist" :key="i">{{todo}}</li>
+      </ul>
+    </section>
+    <button @click="addTodo">Add Todo</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "app"
+  name: "app",
+  data() {
+    return {
+      todo: "",
+      todolist: []
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todolist.push(this.todo);
+      this.todo = "";
+    }
+  }
 };
 </script>
 
