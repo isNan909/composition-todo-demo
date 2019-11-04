144130131d249e4dd719ea159151a40e087b0a2e
Ishan Manandhar <ishan02016@gmail.com>
2019-11-04 09:56:32 +0545

    added remove and add todo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
     <section>
+      <h3>My Todo List</h3>
       <input type="text" v-model="state.todo" />
       <ul>
-        <li v-for="(todo, i) in state.todolist" :key="i">{{todo}}</li>
+        <li v-for="(todo, i) in state.todolist" :key="i">
+          {{todo}}
+          <button @click="removeTodo(i)">x</button>
+        </li>
       </ul>
     </section>
     <button @click="addTodo">Add Todo</button>
@@ -14,10 +18,11 @@
 import { reactive, ref } from "@vue/composition-api";
 export default {
   setup() {
-    const { state, addTodo } = useOurTodo();
+    const { state, addTodo, removeTodo } = useOurTodo();
     return {
       state,
-      addTodo
+      addTodo,
+      removeTodo
     };
   }
 };
@@ -32,9 +37,13 @@ function useOurTodo() {
     state.todolist.push(state.todo);
     state.todo = "";
   }
+  function removeTodo(i) {
+    state.todolist.splice(i);
+  }
   return {
     state,
-    addTodo
+    addTodo,
+    removeTodo
   };
 }
 </script>
